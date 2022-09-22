180b9f993ff15208d48591fbe2ebe2e8e311d297
HaloSense <liuzj990412@live.com>
2022-09-22 02:57:08 -0400

    Add encoder source file

---
+++ segment_digit/segment_digit.srcs/sources_1/new/2_4_encoder.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2022/09/22 02:55:17
+// Design Name: 
+// Module Name: 2_4_encoder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module 2_4_encoder(
+    input [1:0] A,
+    output [3:0] Y
+    );
+endmodule
