7c86a67c802618025e369b6b9a06e13bbea80ec9
HaloSense <liuzj990412@live.com>
2022-12-11 09:44:07 -0500

    3 divider

---
+++ divide3/divide3.srcs/sources_1/new/div3.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2022/11/29 17:33:27
+// Design Name: 
+// Module Name: div3
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module div3(
+    input wire rst, clk,
+    output wire out
+    );
+    
+    reg out1, out2;
+    reg [1:0] count1, count2;
+    
+    always@(posedge clk)
+    begin
+        
+    end
+    
+    
+endmodule
